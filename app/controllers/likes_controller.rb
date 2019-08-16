0013ddbcbbce1b131983f7640fcbb0079892a3ee
kumayk <kumayk1882@gmail.com>
2019-08-16 17:43:42 +0900

    Add/post_image/category

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def create
         post_image = PostImage.find(params[:post_image_id])
         like = current_user.likes.new(post_image_id: post_image.id)
-        like.save
+        like.save!
           redirect_to post_image_path(post_image)
   end
   def destroy
