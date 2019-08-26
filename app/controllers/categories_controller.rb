9c9c4c6bbb3ec5afc116d6030c22be2280211807
kumayk <kumayk1882@gmail.com>
2019-08-27 05:30:03 +0900

    Add/show/layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   def index
   	@category = Category.find(params[:id])
     @categories = Category.all
-    @post_images = PostImage.page(params[:page]).per(9)
+    @post_images = @category.post_images.page(params[:page]).per(6)
   end
   def show
   	@category = Category.find(params[:id])
